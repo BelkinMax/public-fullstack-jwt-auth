ec2f7b322a4002b2b1382cc0c34c8bfdbc3569be
Maxim Belkin <belkinurban@gmail.com>
2021-07-05 19:24:06 +0200

    feat: :sparkles: Connect to mongo ddbb

--- server/index.js
+++ server/index.js
@@ -6,12 +6,25 @@ const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 5000;
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const app = express();
 
 const start = async () => {
   try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.1y6xw.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
