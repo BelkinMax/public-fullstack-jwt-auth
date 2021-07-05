85b01c8feaf72399f564c54b3ceacca9daff01fe
Maxim Belkin <belkinurban@gmail.com>
2021-07-05 19:41:00 +0200

    fix: :bug: Fix .env access

--- server/index.js
+++ server/index.js
@@ -9,16 +9,13 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
-const DB_NAME = process.env.DB_NAME;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const app = express();
 
 const start = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.1y6xw.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1y6xw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
