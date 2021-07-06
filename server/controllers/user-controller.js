18180390c907b799a913d4bc9beba17a952fa482
Maxim Belkin <belkinurban@gmail.com>
2021-07-06 11:13:00 +0200

    feat: :sparkles: Create registration controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,6 +1,19 @@
+const UserService = require('../service/user-service');
+
 class UserController {
   async registration(req, res, next) {
     try {
+      const { email, password } = req.body;
+      const userData = await UserService.registration({ email, password });
+
+      const maxAge = 30 * 24 * 60 * 60 * 1000; // 30 days
+
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
     } catch (e) {
       console.error(e);
     }
