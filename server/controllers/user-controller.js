7beb43807db00a24a1896c55792d8e57653e1438
Maxim Belkin <belkinurban@gmail.com>
2021-07-06 10:13:19 +0200

    feat: :sparkles: Create controller

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,46 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      res.json('Hello world!');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+module.exports = new UserController();
