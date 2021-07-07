ac7b9b68630ea412b3502f161b3795fb7484294d
Maxim Belkin <belkinurban@gmail.com>
2021-07-07 23:23:40 +0200

    style: :lipstick: Create a login form

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>
-    <div>
-      <h1>
-        Hello world
-      </h1>
-    </div>
-  </div>
+  <main
+    class="h-screen mx-auto p-4 pb-20 bg-gradient-to-r from-blue-400 to-orange-500 via-purple-500 animate-gradient-x flex flex-col items-center justify-center text-gray-700"
+  >
+    <section
+      class="bg-gray-100 p-6 rounded-xl shadow-lg w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
+    >
+      <div class="mb-6">
+        <input
+          class="mb-4 p-2 appearance-none block w-full bg-gray-200 placeholder-gray-900 rounded border focus:border-teal-500"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          class="mb-4 p-2 appearance-none block w-full bg-gray-200 placeholder-gray-900 rounded border focus:border-teal-500"
+          type="text"
+          placeholder="Password"
+        />
+
+        <div class="flex items-center">
+          <div class="w-1/2 flex items-center">
+            <input id="remember-me" type="checkbox" class="mr-2" />
+            <label for="remember-me">Remember me!</label>
+          </div>
+          <button
+            class="ml-auto w-1/2 bg-gray-800 text-white p-2 rounded font-semibold hover:bg-gray-900"
+            type="submit"
+          >
+            Log In
+          </button>
+        </div>
+      </div>
+
+      <div class="text-center flex justify-between w-full">
+        <a
+          class="text-sm font-bold text-teal-500 hover:underline cursor-pointer"
+          >Create an accout</a
+        >
+        <a
+          class="text-sm font-bold text-teal-500 hover:underline cursor-pointer"
+          >Forgot your password?</a
+        >
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,22 +1,62 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        "gradient-x": "gradient-x 30s ease infinite",
+        "gradient-y": "gradient-y 30s ease infinite",
+        "gradient-xy": "gradient-xy 30s ease infinite"
+      },
+      keyframes: {
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center"
+          }
+        },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center"
+          }
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center"
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center"
+          }
+        }
+      }
+    }
+  },
   variants: {},
   plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === "production",
     content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
+      "components/**/*.vue",
+      "layouts/**/*.vue",
+      "pages/**/*.vue",
+      "plugins/**/*.js",
+      "nuxt.config.js"
     ]
   }
-}
+};
