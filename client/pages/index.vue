e529cfa18bb88436d3f6c339e691f263a4a52223
Maxim Belkin <belkinurban@gmail.com>
2021-07-09 11:48:58 +0200

    feat: :sparkles: Create user card component

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <router-link to="/login">Login</router-link>
+  <div
+    class="w-full h-full grid grid-flow-row grid-cols-4 grid-rows-6 gap-4 p-4"
+  >
+    <UserCard v-for="user in users" :key="user.id" :user="user" />
   </div>
 </template>
 
 <script>
-export default {};
+import UserCard from "@/components/UserCard.vue";
+
+export default {
+  components: { UserCard },
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Lorem Ipsum",
+          email: "test@test.com"
+        },
+        {
+          id: 2,
+          name: "Hello World",
+          email: "hello@world.com"
+        },
+        {
+          id: 3,
+          name: "User Name",
+          email: "user@name.com"
+        }
+      ]
+    };
+  }
+};
 </script>
